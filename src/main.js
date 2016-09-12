c50c3a1baba23cb88ac2c30f4ab065572d2d4640
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-11 20:47:32 -0700

    stuff

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
+import React from 'react'
+import ReactDom from 'react-dom'
 
 ReactDom.render(
-  <h1>Hello World!</h1>,
+  <h1>Hello World</h1>,
   document.getElementById('root')
-);
+)
